09e4599917f9e7f251257151b9f25405014672fb
Alex Luan <songdingxx@gmail.com>
2021-02-16 01:55:17 -0500

    Seperate logic for processing inputs

--- AppHistory.py
+++ AppHistory.py
@@ -1,45 +1,10 @@
-import pymongo
-from pymongo import MongoClient
-from ConnDB import *
+from ProcessInput import *
+from ConnDB import getConnection, getDatabase, getCollection, closeConnection
 
-def insertVal(collection, post):
-    try:
-        collection.insert_one(post)
-    except:
-        print("Insertion failed")
-
-def checkParamsLength(parms, l):
-    if len(parms) < l:
-        print("\n(Parameters Error) Insufficient Parameters\n")
-        return False
-    return True
-
-# Process input
-def processInput(command, parms):
-    # Insertion
-    if command == "insert":
-        if (checkParamsLength(parms, 3)):
-            print("Insert")
-        return
-    # Counting
-    if command == "count":
-        if (checkParamsLength(parms, 2)):
-            print("Count")
-        return
-    # Update Status by JobId
-    if command == "updateStatusById":
-        if (checkParamsLength(parms, 3)):
-            print("updateStatusById")
-        return
-    # Update Status by Position
-    if command == "updateStatusByPos":
-        if (checkParamsLength(parms, 3)):
-            print("updateStatusByPos")
-        return
-    # Update Status by Position
-    if command == "Total":
-        print("Total")
-        return
+MONGODB_HOST = "localhost"
+MONGODB_PORT = 27017
+DB_NAME = "ApplicationHistory"
+COLLECTION_NAME = "histories"
 
 def main():
     conn = getConnection(MONGODB_HOST, MONGODB_PORT)
@@ -49,7 +14,7 @@ def main():
     while(True):
         command = input("Please type your command\n")
         if command == "quit": 
-            closeConnectio(conn)
+            closeConnection(conn)
             break
         if command == "help":
             print()
